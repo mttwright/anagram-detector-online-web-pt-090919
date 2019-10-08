b3b8e1eff5879eb4ea5e1edd1ff7564bcd03aee3
virtual-frame-7789 <mttwright@gmail.com>
2019-10-08 23:36:25 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    matches = []
+    words.each do |word|
+      if word.split(//).sort == self.word.split(//).sort
+        matches.push(word)
+      end
+    end
+    matches
+  end
+      
+  
+  
+  
+end
